8a7d6a2933535c13d45226ec25dd796703f83357
jjaybah <d.jaylah@gmail.com>
2024-11-28 10:08:19 +0000

    add GET /api/users/:username

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,16 @@
-const { selectUsers } = require("../models/users.model");
+const { selectUsers, selectUserByUsername } = require("../models/users.model");
 
 exports.getUsers = (req, res) => {
   selectUsers().then((users) => {
     res.status(200).send({ users });
   });
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;
+  selectUserByUsername(username)
+    .then((user) => {
+      res.status(200).send({ user });
+    })
+    .catch(next);
 };
